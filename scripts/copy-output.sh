179f9cdc58c0c1b54021d007c5fb591ac2b638d3
SirStendec <sir@stendec.me>
2024-10-12 15:18:59 -0400

    Remove the webpack manifest files from the output, since Chrome at a minimum complains.

--- scripts/copy-output.sh
+++ scripts/copy-output.sh
@@ -7,8 +7,12 @@ cp -r client/dist/* dist/web/
 cp -r addons/dist/* dist/web/
 cp -rf src/* dist/
 
+# Remove some files that we don't want to include
+# in our packaged extensions.
 rm -rf dist/web/bom
 rm -rf dist/web/addons-bom
+rm dist/web/manifest.json
+rm dist/web/addons/manifest.json
 
 # Find the addons.*.json file and rename it.
 shopt -s nocasematch
