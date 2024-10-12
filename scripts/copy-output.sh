12ba5d61f367719f38df301b523b2ce3be7b0013
SirStendec <sir@stendec.me>
2024-10-12 14:43:26 -0400

    Fix a typo in testing manifests. Change the Firefox add-on id in testing manifests. Remove SBOM from built archives to avoid differences in builds that Mozilla will inevitably whine about.

--- scripts/copy-output.sh
+++ scripts/copy-output.sh
@@ -7,6 +7,9 @@ cp -r client/dist/* dist/web/
 cp -r addons/dist/* dist/web/
 cp -rf src/* dist/
 
+rm -rf dist/web/bom
+rm -rf dist/web/addons-bom
+
 # Find the addons.*.json file and rename it.
 shopt -s nocasematch
 for file in dist/web/*; do
--- scripts/testing-manifest.js
+++ scripts/testing-manifest.js
@@ -5,6 +5,9 @@ const manifest = JSON.parse(fs.readFileSync('dist/manifest.json'));
 manifest.name = 'FrankerFaceZ Testing';
 manifest.short_name = 'FFZ-Testing';
 
-manifest.description = `This is the testing Version of FrankerFaceZ. ${manifest.description}`;
+manifest.description = `This is the testing version of FrankerFaceZ. ${manifest.description}`;
+
+if (manifest?.browser_specific_settings?.gecko)
+    manifest.browser_specific_settings.gecko.id = 'testing@frankerfacez.com';
 
 fs.writeFileSync('dist/manifest.json', JSON.stringify(manifest, null, '\t'));
