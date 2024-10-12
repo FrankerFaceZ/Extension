306ddbc3ed61b64275ffb6ac2fa0ec4032169f9f
SirStendec <sir@stendec.me>
2024-10-12 15:25:08 -0400

    Chrome really gettin on my nerves right now.

--- scripts/nightly-manifest.js
+++ scripts/nightly-manifest.js
@@ -5,7 +5,7 @@ const manifest = JSON.parse(fs.readFileSync('dist/manifest.json'));
 manifest.name = 'FrankerFaceZ Nightly';
 manifest.short_name = 'FFZ-Nightly';
 
-manifest.description = `THIS IS THE NIGHTLY BUILD OF FRANKERFACEZ. This extension is for TESTING. Users who aren't interested in testing should use the stable extension.  ${manifest.description}`;
+manifest.description = `This is the nightly testing build of FrankerFaceZ. FrankerFaceZ is an enhancement suite for Twitch.`;
 
 if (manifest?.browser_specific_settings?.gecko)
     manifest.browser_specific_settings.gecko.id = 'nightly@frankerfacez.com';
